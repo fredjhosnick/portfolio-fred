fb6e26171a7b4628910b258bda3fce97d8f6d8d6
fredjhosnick <36901848+fredjhosnick@users.noreply.github.com>
2023-04-30 17:00:27 +0200

    Update ProjectList.js

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -3,6 +3,7 @@ import Proj2 from '../assets/proj2.jpg'
 import Proj3 from '../assets/proj3.jpg'
 import Proj4 from '../assets/proj4.jpg'
 import Proj5 from '../assets/proj5.jpg'
+import Proj6 from '../assets/proj6.jpg'
 
 const ProjectList = [
   
@@ -41,6 +42,14 @@ const ProjectList = [
           skills:"Javascript,React.js",
           email:"https://todo-list-delta-peach.vercel.app/",
           github:"https://github.com/fredjhosnick/todo-list"
+     
+       },
+  {
+         name:"Movie-List",
+          image:Proj6,
+          skills:"Javascript,React.js",
+          email:"https://movie-list-tan.vercel.app/",
+          github:"https://github.com/fredjhosnick/movie-list"
      
        }
 ]
